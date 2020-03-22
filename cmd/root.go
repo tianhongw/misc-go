304d7053c35811c8d568da54974a67c98943962b
tianhongw <dream@trytwice.me>
2020-03-22 22:44:52 +0800

    add cmd module

---
+++ cmd/root.go
@@ -0,0 +1,78 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/tianhongw/misc-go/conf"
+)
+
+const (
+	defaultCfgFile = "$HOME/.app.toml"
+	defaultCfgType = "toml"
+)
+
+var (
+	cfgFile string
+	cfgType string
+)
+
+var rootCmd = cobra.Command{
+	Use: "app name",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("config file used: %s", cfgFile)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		run()
+	},
+}
+
+func init() {
+	cobra.OnInitialize(doInit)
+
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", fmt.Sprintf("config file path, default use: %s", defaultCfgFile))
+	rootCmd.PersistentFlags().StringVarP(&cfgType, "type", "t", "", fmt.Sprintf("config file type, default use: %s", defaultCfgType))
+	rootCmd.PersistentFlags().StringP("log", "l", "", "log file path")
+
+	viper.BindPFlag("log", rootCmd.PersistentFlags().Lookup("log"))
+}
+
+func doInit() {
+	if cfgFile == "" {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Errorf("failed to get home dir: %v", err)
+			os.Exit(1)
+		}
+		cfgFile = strings.Replace(defaultCfgFile, "$HOME", home, 1)
+	}
+
+	if cfgType == "" {
+		cfgType = defaultCfgType
+	}
+
+	if cfgFileUsed, err := conf.Init(cfgFile, cfgType); err != nil {
+		fmt.Errorf("failed to load config file: %v", err)
+		os.Exit(1)
+	} else {
+		cfgFile = cfgFileUsed
+	}
+}
+
+func run() {
+	if err := conf.Opts.Validate(); err != nil {
+		fmt.Errorf("invalid config: %v", err)
+		os.Exit(1)
+	}
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Errorf("execute failed: %v", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.2.2 // indirect
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/cobra v0.0.6
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.6.2
